e270602d3d42071108a465d9c0ab20c04a2fa8f9
NOBITA <nobitaanetwork@gmail.com>
2025-07-06 00:30:59 +0530

    Update myinfo.py

--- MukeshRobot/modules/myinfo.py
+++ MukeshRobot/modules/myinfo.py
@@ -10,13 +10,13 @@ from MukeshRobot import telethn as tgbot
 from MukeshRobot.events import register
 
 edit_time = 5
-""" =======================𝗩𝗜𝗣 𝗥𝗢𝗕𝗢𝗧====================== """
+""" =======================𝗥𝗢𝗕𝗢𝗧====================== """
 file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
 file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
 file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
 file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
 file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
-""" =======================𝗩𝗜𝗣 𝗥𝗢𝗕𝗢𝗧====================== """
+""" =======================𝗥𝗢𝗕𝗢𝗧====================== """
 
 
 @register(pattern="/myinfo")
@@ -82,4 +82,4 @@ __mod_name__ = "📍ɪɴғᴏ📍"
 __help__ = """
  /myinfo  ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ɪɴғᴏ 
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆"""
+☆............𝙱𝚈 » [⏤͟͟͞͞𓆩 𝐁ᴏᴛ ꭙ 𝐌ᴀᴋᴇʀ𓆪](https://t.me/AboutBotMaker)............☆"""
