537deffc2744870f13f1137b48f030e1a7f84d53
NOBITA <nobitaanetwork@gmail.com>
2025-07-06 00:23:13 +0530

    Update sexy.py

--- MukeshRobot/modules/sexy.py
+++ MukeshRobot/modules/sexy.py
@@ -4,7 +4,7 @@ from telethon import Button, events
 
 from .. import telethn as asst
 
-BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/TG_FRIENDSS")]]
+BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/AboutBotMaker")]]
 HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
 SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
 LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
